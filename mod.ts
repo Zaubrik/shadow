4cd9b1adbb2b7d8cfc0bf7b41d0e0768ec3f2518
timonson <timo.rossa@tutanota.com>
2021-11-29 04:14:27 +0100

    Export 'createHtmlTemplate'

--- mod.ts
+++ mod.ts
@@ -6,4 +6,6 @@ export { css } from "./css.ts";
 
 export { customElement, property } from "./decorators.ts";
 
+export { createHtmlTemplate } from "./util.ts";
+
 export type { Attribute } from "./shadow.ts";
