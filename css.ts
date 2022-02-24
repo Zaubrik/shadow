96f6742da6862a23bda0fb1644aabd5fea57aa2a
timonson <timo.rossa@tutanota.com>
2022-02-25 00:56:14 +0100

    Allow recursive function call

--- css.ts
+++ css.ts
@@ -6,7 +6,7 @@ import { createTemplate } from "./util.ts";
  */
 export function css(
   strings: TemplateStringsArray,
-  ...values: (string | HTMLTemplateElement)[]
+  ...values: (string | HTMLTemplateElement | HTMLTemplateElement[])[]
 ): HTMLTemplateElement[] {
   const cssTemplates = [];
   cssTemplates.push(
@@ -15,6 +15,11 @@ export function css(
         (acc, value, i) => {
           if (value instanceof HTMLTemplateElement) {
             cssTemplates.push(value);
+            return acc + strings[i + 1];
+          } else if (
+            Array.isArray(value)
+          ) {
+            value.forEach((el) => cssTemplates.push(el));
             return acc + strings[i + 1];
           } else {
             return acc + value + strings[i + 1];
